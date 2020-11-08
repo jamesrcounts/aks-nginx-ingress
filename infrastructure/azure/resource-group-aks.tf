9ef2277a706370fe6cc93629ac1dc75656ae8850
Jim Counts <jamesrcounts@outlook.com>
2020-11-08 14:48:29 +0000

    change strategy for finding aks node group id

--- infrastructure/azure/resource-group-aks.tf
+++ infrastructure/azure/resource-group-aks.tf
@@ -1,15 +1,19 @@
 resource "azurerm_role_assignment" "aks_nodes_managed_identity_operator" {
   principal_id         = local.aks_system_assigned_principal_id
   role_definition_name = "Managed Identity Operator"
-  scope                = data.azurerm_resource_group.aks_nodes.id
+  scope                = local.node_resource_group_id
 }
 
 resource "azurerm_role_assignment" "aks_nodes_virtual_machine_contributor" {
   principal_id         = local.aks_system_assigned_principal_id
   role_definition_name = "Virtual Machine Contributor"
-  scope                = data.azurerm_resource_group.aks_nodes.id
+  scope                = local.node_resource_group_id
 }
 
-data "azurerm_resource_group" "aks_nodes" {
-  name = azurerm_kubernetes_cluster.aks.node_resource_group
+locals {
+  node_resource_group_id = replace(
+    azurerm_resource_group.main.id,
+    azurerm_resource_group.main.name,
+    azurerm_kubernetes_cluster.aks.node_resource_group
+  )
 }
